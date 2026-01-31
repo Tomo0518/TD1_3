bbf5337bf2ceab10bb1c494f17d1fe562d2bc406
Tomo0518 <tomo.mura0018@gmail.com>
2026-01-31 19:16:51 +0900

    stage1をよしかわさんのに更新

--- TD1_3/main.cpp
+++ TD1_3/main.cpp
@@ -41,7 +41,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	auto& mapData = MapData::GetInstance();
 	mapData.Load("./Resources/data/stage1.json");
 
-
 	// キー入力結果を受け取る箱 
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
